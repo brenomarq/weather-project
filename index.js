21a2dc49c84f83573824606ec72d25ca2a726f0e
brenomarq <brenomarques516@gmail.com>
2023-12-31 09:07:31 -0300

    Implement some routes and index.ejs

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
